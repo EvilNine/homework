1c1992bb00f136edb6157a350fb41675085350ab
Aleksandr Bolshakov <alexbolshakov2@gmail.com>
2022-11-15 15:18:09 +0300

    homework 2

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -4,25 +4,18 @@ import {AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
 
 type AffairsPropsType = {
-    data: any // need to fix any
-    setFilter: any
-    deleteAffairCallback: any
+    data: Array<AffairType> // need to fix any
+    setFilter: (value: string)=> void
+    deleteAffairCallback: (id: number)=> void
     filter: FilterType
 }
 
 function Affairs(props: AffairsPropsType) {
-    const setAll = () => {
-        // need to fix
-    }
-    const setHigh = () => {
-        // need to fix
-    }
-    const setMiddle = () => {
-        // need to fix
-    }
-    const setLow = () => {
-        // need to fix
-    }
+
+    const setAll = () => props.setFilter('all')
+    const setHigh = () => props.setFilter('high')
+    const setMiddle = () => props.setFilter('middle')
+    const setLow = () => props.setFilter('low')
 
     const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
     const cnHigh = s.button + ' ' + s.high + (props.filter === 'high' ? ' ' + s.active : '')
@@ -30,42 +23,32 @@ function Affairs(props: AffairsPropsType) {
     const cnLow = s.button + ' ' + s.low + (props.filter === 'low' ? ' ' + s.active : '')
 
     const mappedAffairs = props.data.map((a: AffairType) => (
-        <Affair
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
-            affair={a}
-            deleteAffairCallback={props.deleteAffairCallback}
-        />
+        <Affair key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+                affair={a}
+                deleteAffairCallback={props.deleteAffairCallback}/>
     ))
 
     return (
         <div>
             <div className={s.buttonContainer}>
-                <button
-                    id={'hw2-button-all'}
-                    onClick={setAll}
-                    className={cnAll}
-                >
+                <button id={'hw2-button-all'}
+                        onClick={setAll}
+                        className={cnAll}>
                     All
                 </button>
-                <button
-                    id={'hw2-button-high'}
-                    onClick={setHigh}
-                    className={cnHigh}
-                >
+                <button id={'hw2-button-high'}
+                        onClick={setHigh}
+                        className={cnHigh}>
                     High
                 </button>
-                <button
-                    id={'hw2-button-middle'}
-                    onClick={setMiddle}
-                    className={cnMiddle}
-                >
+                <button id={'hw2-button-middle'}
+                        onClick={setMiddle}
+                        className={cnMiddle}>
                     Middle
                 </button>
-                <button
-                    id={'hw2-button-low'}
-                    onClick={setLow}
-                    className={cnLow}
-                >
+                <button id={'hw2-button-low'}
+                        onClick={setLow}
+                        className={cnLow}>
                     Low
                 </button>
             </div>
--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -6,40 +6,35 @@ import s2 from '../Affairs.module.css'
 type AffairPropsType = {
     // key не нужно типизировать
     affair: AffairType
-    deleteAffairCallback: any // need to fix any
+    deleteAffairCallback: (id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {
-        // need to fix
-    }
+
+    const deleteCallback = () => props.deleteAffairCallback(props.affair._id)
 
     const nameClass = s.name + ' ' + s2[props.affair.priority]
     const buttonClass = s.closeButton + ' ' + s2[props.affair.priority]
     const affairClass = s.affair + ' ' + s2[props.affair.priority]
 
     return (
-        <div
-            id={'hw2-affair-' + props.affair._id}
-            className={affairClass}
-        >
+        <div id={'hw2-affair-' + props.affair._id}
+             className={affairClass}>
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
                 {/*создаёт студент*/}
-
+                {props.affair.name}
                 {/**/}
             </div>
             <div id={'hw2-priority-' + props.affair._id} hidden>
                 {/*создаёт студент*/}
-
+                {props.affair.priority}
                 {/**/}
             </div>
 
-            <button
-                id={'hw2-button-delete-' + props.affair._id}
-                className={buttonClass}
-                // need to fix
-
-            >
+            <button id={'hw2-button-delete-' + props.affair._id}
+                    className={buttonClass}
+                    // need to fix
+                    onClick={deleteCallback}>
                 {/*текст кнопки могут изменить студенты*/}
                 X
                 {/**/}
