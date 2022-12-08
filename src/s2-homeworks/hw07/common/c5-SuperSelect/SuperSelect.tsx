1bbe51d5532bc1790c19f4677bdbf7a59f592fab
Aleksandr Bolshakov <alexbolshakov2@gmail.com>
2022-12-08 16:19:48 +0300

    homework 5

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -36,6 +36,7 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
         : [] // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+        
         // делают студенты
     }
 
