8399f8d073bf3283dfac9e3e1a72778b38ba6d33
Aleksandr Bolshakov <alexbolshakov2@gmail.com>
2022-12-11 11:22:20 +0300

    hw5

--- src/s2-homeworks/hw05/HW5.tsx
+++ src/s2-homeworks/hw05/HW5.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter } from 'react-router-dom'
+import { HashRouter, BrowserRouter } from 'react-router-dom'
 import { Layout } from './layout/Layout'
 import Pages from './Pages'
 
@@ -11,12 +11,12 @@ import Pages from './Pages'
 
 function HW5() {
     return (
-        <HashRouter>
+        <BrowserRouter>
             {/*в gh-pages лучше работает HashRouter, с BrowserRouter скорее всего не пройдёт тест*/}
             <Layout>
                 <Pages />
             </Layout>
-        </HashRouter>
+        </BrowserRouter>
     )
 }
 
