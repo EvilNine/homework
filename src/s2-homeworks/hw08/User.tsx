90cc87a42f3601710eaf8a1c17c80e68af485cb1
Aleksandr Bolshakov <alexbolshakov2@gmail.com>
2023-01-02 18:55:07 +0300

    hw8

--- src/s2-homeworks/hw08/User.tsx
+++ src/s2-homeworks/hw08/User.tsx
@@ -8,13 +8,16 @@ type UserPropsType = {
 }
 
 const User: React.FC<UserPropsType> = ({ u }) => {
+    
     return (
         <tr id={'hw8-user-' + u._id + '-' + u.age} className={s.item}>
             <td id={'hw8-user-name-' + u._id} className={s.nameCol}>
+                {u.name}
                 {/*отобразить имя*/}
 
             </td>
-            <td id={'hw8-user-age-' + u._id}>
+            <td id={'hw8-user-age-' + u._id} className={s.ageCol}>
+                {u.age}
                 {/*отобразить возраст*/}
 
             </td>
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,15 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            return [...state].sort((a, b) => (
+                action.payload === 'up' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+            ))
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(item => item.age >= action.payload) // need to fix
         }
         default:
             return state
