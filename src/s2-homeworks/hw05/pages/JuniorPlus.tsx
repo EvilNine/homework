ece180a09d09d2cebe2aaa2400a92fccec94d64d
Aleksandr Bolshakov <alexbolshakov2@gmail.com>
2023-02-12 14:44:14 +0300

    hw13

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -3,7 +3,8 @@ import HW10 from '../../hw10/HW10'
 import HW11 from '../../hw11/HW11'
 import HW12 from '../../hw12/HW12'
 import HW13 from '../../hw13/HW13'
-
+import HW14 from '../../hw14/HW14'
+import HW15 from '../../hw15/HW15'
 function JuniorPlus() {
     return (
         <div id={'hw5-page-junior-plus'}>
@@ -11,6 +12,8 @@ function JuniorPlus() {
             <HW11 />
             <HW12 />
             <HW13 />
+            <HW14 />
+            {/*<HW15 />*/}
         </div>
     )
 }
